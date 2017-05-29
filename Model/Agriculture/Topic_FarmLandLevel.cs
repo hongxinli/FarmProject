95b21c28133c5e0a9d6e89ebe95c36625c1378b4
Coderli <hongxin1989@foxmail.com>
2017-05-29 11:36:01 +0800

    地力等级图接口

--- Model/Agriculture/Topic_FarmLandLevel.cs
+++ Model/Agriculture/Topic_FarmLandLevel.cs
@@ -7,20 +7,41 @@ namespace Model.Agriculture
 {
     public class Topic_FarmLandLevel
     {
+        /// <summary>
+        /// 城市
+        /// </summary>
         public string city { get; set; }
-
+        /// <summary>
+        /// 城市总面积
+        /// </summary>
         public string cityArea { get; set; }
-
+        /// <summary>
+        /// 城市等级面积
+        /// </summary>
         public string cityLevelArea { get; set; }
+        /// <summary>
+        /// 乡镇
+        /// </summary>
         public string town { get; set; }
-
+        /// <summary>
+        /// 乡镇总面积
+        /// </summary>
         public string townArea { get; set; }
-
+        /// <summary>
+        /// 乡镇等级面积
+        /// </summary>
         public string townLevelArea { get; set; }
+        /// <summary>
+        /// 村落
+        /// </summary>
         public string village { get; set; }
-
+        /// <summary>
+        /// 村落总面积
+        /// </summary>
         public string villageArea { get; set; }
-
+        /// <summary>
+        /// 村落等级面积
+        /// </summary>
         public string villageLevelArea { get; set; }
     }
 }
