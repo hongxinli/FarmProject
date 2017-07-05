c3e0a386233134e27002d8c09b570453cbf7ce1b
Coderli <hongxin1989@foxmail.com>
2017-07-06 07:50:21 +0800

    修改地图服务

---
+++ Model/Agriculture/ChartData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model.Agriculture
+{
+    public class ChartData
+    {
+        public List<string> labels { get; set; }
+
+        public List<BarSets> data { get; set; }
+    }
+    public class BarSets
+    {
+        public string fillColor { get; set; }
+        public string strokeColor { get; set; }
+        public List<decimal> data { get; set; }
+    }
+}
--- WebGis/js/initMap.js
+++ WebGis/js/initMap.js
@@ -11,15 +11,14 @@ dojo.require("esri.tasks.IdentifyParameters");
 dojo.require("esri.tasks.IdentifyTask");
 dojo.require("esri.tasks.FindTask");
 dojo.require("esri.tasks.FindParameters");
-var dataLayerUrl = "http://36.7.89.33:6081/arcgis/rest/services/贵池区土地分布汇总图/MapServer";
-var baseLayerUrl = "http://36.7.89.33:6081/arcgis/rest/services/贵池区行政区划图/MapServer";
+var dataLayerUrl = "http://36.7.89.33:6081/arcgis/rest/services/施肥分区图/MapServer";
+var baseLayerUrl = "http://36.7.89.33:6081/arcgis/rest/services/安徽省行政区划图/MapServer";
 var geometryServiceUrl = "http://36.7.89.33:6081/arcgis/rest/services/Utilities/Geometry/GeometryServer";
-var legendUrl = "http://36.7.89.33:6081/arcgis/rest/services/贵池区土地分布汇总图/MapServer/legend?f=pjson";
+var legendUrl = "http://36.7.89.33:6081/arcgis/rest/services/施肥分区图/MapServer/legend?f=pjson";
 var navToolbar = null;
 var map, imgLayer, dataLayer, baselayer, geometryService;
 function init() {
-    //初始化地图范围
-   // initPage();
+    
     //地图初始化
     map = new esri.Map("mapDiv", { logo: false, nav: false });
     //比例尺
@@ -28,12 +27,13 @@ function init() {
         scalebarStyle: "line",
         scalebarUnit: "metric"
     }, dojo.byId("scaleBarDiv"));
-    //data图层添加
-    dataLayer = new esri.layers.ArcGISDynamicMapServiceLayer(dataLayerUrl, { id: "dataLayer" });
-    map.addLayer(dataLayer);
+   
     //base图层添加
     baselayer = new esri.layers.ArcGISDynamicMapServiceLayer(baseLayerUrl, { id: "baselayer" });
-    map.addLayer(baselayer);
+    map.addLayer(baselayer, 1);
+    //data图层添加
+    dataLayer = new esri.layers.ArcGISDynamicMapServiceLayer(dataLayerUrl, { id: "dataLayer" });
+    map.addLayer(dataLayer, 0);
     //初始化geo服务
     geometryService = new esri.tasks.GeometryService(geometryServiceUrl);
     geometryService.on("lengths-complete", outputLength);
